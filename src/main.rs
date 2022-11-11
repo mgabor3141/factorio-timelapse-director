e0271cf0b476584f757c08f1ea3b503201382f3f
mgabor3141 <>
2022-11-11 13:46:12 +0100

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use csv;
+use ggez::conf::{WindowMode, WindowSetup};
 use ggez::{
     event,
     glam::*,
     graphics::{self, Color},
-    Context, GameResult, winit::{dpi::LogicalSize}, mint::Point2, input
+    input,
+    mint::Point2,
+    winit::dpi::LogicalSize,
+    Context, GameResult,
 };
-use ggez::conf::{WindowMode, WindowSetup};
-use std::{io};
-use csv;
 use serde::Deserialize;
+use std::io;
 
 struct MainState {
     mouse_down: bool,
@@ -15,7 +18,7 @@ struct MainState {
     zoom: f32,
     playing: bool,
     time: u64,
-    events: Vec<Event>
+    events: Vec<Event>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -35,19 +38,19 @@ impl MainState {
             // error here.
             let event: Event = match result {
                 Ok(r) => r,
-                Err(_) => panic!("Error loading CSV")
+                Err(_) => panic!("Error loading CSV"),
             };
 
             events.push(event)
-        }    
+        }
 
         Ok(MainState {
             mouse_down: false,
-            pan: Point2{x: 0.0, y: 0.0},
+            pan: Point2 { x: 0.0, y: 0.0 },
             zoom: 1.0,
             playing: false,
             events,
-            time: 0
+            time: 0,
         })
     }
 }
@@ -64,32 +67,27 @@ impl event::EventHandler<ggez::GameError> for MainState {
         let mut canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
 
-        
-        let mut instance_array = graphics::InstanceArray::new(
-            ctx,
-            None
-        );
+        let mut instance_array = graphics::InstanceArray::new(ctx, None);
 
         let events = &self.events;
         let zoom_factor = f32::powf(2.0, self.zoom);
         let scale_factor = ctx.gfx.window().scale_factor();
 
         let mut i = 0;
-        while i < events.len() && events[i].tick < self.time  {
+        while i < events.len() && events[i].tick < self.time {
             let event = &events[i];
-            instance_array.push(graphics::DrawParam::new()
-                .dest(Vec2::new(
-                    event.x * scale_factor as f32 * zoom_factor + self.pan.x,
-                    event.y * scale_factor as f32 * zoom_factor + self.pan.y
-                ))
-            );
+            instance_array.push(graphics::DrawParam::new().dest(Vec2::new(
+                event.x * scale_factor as f32 * zoom_factor + self.pan.x,
+                event.y * scale_factor as f32 * zoom_factor + self.pan.y,
+            )));
 
             i += 1
         }
 
         canvas.draw(&instance_array, Vec2::new(800.0, 800.0));
 
-        let fps_display = graphics::Text::new(format!("FPS: {:.0} Time: {:.0}", ctx.time.fps(), self.time));
+        let fps_display =
+            graphics::Text::new(format!("FPS: {:.0} Time: {:.0}", ctx.time.fps(), self.time));
         canvas.draw(
             &fps_display,
             graphics::DrawParam::from([10.0, 10.0]).color(Color::WHITE),
@@ -102,7 +100,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
 
     fn mouse_wheel_event(&mut self, _ctx: &mut Context, _x: f32, y: f32) -> GameResult {
         self.zoom += y / 20.0;
-        
+
         Ok(())
     }
 
@@ -111,7 +109,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
         _ctx: &mut Context,
         button: event::MouseButton,
         _x: f32,
-        _y: f32
+        _y: f32,
     ) -> GameResult {
         if button == event::MouseButton::Left {
             self.mouse_down = true;
@@ -125,7 +123,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
         _ctx: &mut Context,
         button: event::MouseButton,
         _x: f32,
-        _y: f32
+        _y: f32,
     ) -> GameResult {
         if button == event::MouseButton::Left {
             self.mouse_down = false;
@@ -140,12 +138,15 @@ impl event::EventHandler<ggez::GameError> for MainState {
         _x: f32,
         _y: f32,
         dx: f32,
-        dy: f32
+        dy: f32,
     ) -> GameResult {
         if self.mouse_down {
-            self.pan = Point2{x: self.pan.x + dx, y: self.pan.y + dy};
+            self.pan = Point2 {
+                x: self.pan.x + dx,
+                y: self.pan.y + dy,
+            };
         }
-        
+
         Ok(())
     }
 
@@ -153,7 +154,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
         &mut self,
         ctx: &mut Context,
         input: input::keyboard::KeyInput,
-        _repeated: bool
+        _repeated: bool,
     ) -> GameResult {
         match input.keycode {
             Some(input::keyboard::KeyCode::Space) => self.playing = !self.playing,
@@ -168,13 +169,11 @@ impl event::EventHandler<ggez::GameError> for MainState {
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("points-framer", "ggez")
         .window_setup(WindowSetup::default().title("Factorio Replay Events Points Scatter"))
-        .window_mode(
-            WindowMode {
-                logical_size: Some(LogicalSize::new(800.0, 600.0)),
-                resizable: true,
-                ..Default::default()
-            },
-        );
+        .window_mode(WindowMode {
+            logical_size: Some(LogicalSize::new(800.0, 600.0)),
+            resizable: true,
+            ..Default::default()
+        });
     let (mut ctx, event_loop) = cb.build()?;
     let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
