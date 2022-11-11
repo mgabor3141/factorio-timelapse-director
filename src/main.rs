63cba04c948d6817a0415c114603087c28366341
mgabor3141 <>
2022-11-11 13:29:56 +0100

    add zoom and pan

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use ggez::{
     event,
     glam::*,
     graphics::{self, Color},
-    Context, GameResult
+    Context, GameResult, winit::{dpi::LogicalSize}, mint::Point2
 };
 use ggez::conf::{WindowMode, WindowSetup};
-use std::io;
+use std::{io};
 use csv;
 use serde::Deserialize;
 
 struct MainState {
+    mouse_down: bool,
+    pan: Point2<f32>,
+    zoom: f32,
     time: u64,
     events: Vec<Event>
 }
@@ -22,7 +25,7 @@ struct Event {
 }
 
 impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
+    fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let mut events = Vec::new();
 
         let mut rdr = csv::Reader::from_reader(io::stdin());
@@ -37,7 +40,13 @@ impl MainState {
             events.push(event)
         }    
 
-        Ok(MainState { events, time: 0 })
+        Ok(MainState {
+            mouse_down: false,
+            pan: Point2{x: 0.0, y: 0.0},
+            zoom: 1.0,
+            events,
+            time: 0
+        })
     }
 }
 
@@ -64,8 +73,12 @@ impl event::EventHandler<ggez::GameError> for MainState {
         while i < events.len() && events[i].tick < self.time  {
             let event = &events[i];
 
+            let zoom_factor = f32::powf(2.0, self.zoom);
             instance_array.push(graphics::DrawParam::new()
-                .dest(Vec2::new(event.x, event.y))
+                .dest(Vec2::new(
+                    event.x * ctx.gfx.window().scale_factor() as f32 * zoom_factor + self.pan.x,
+                    event.y * ctx.gfx.window().scale_factor() as f32 * zoom_factor + self.pan.y
+                ))
             );
 
             i += 1
@@ -73,14 +86,63 @@ impl event::EventHandler<ggez::GameError> for MainState {
 
         canvas.draw(&instance_array, Vec2::new(800.0, 800.0));
 
-        let fps_display = graphics::Text::new(format!("FPS: {:.0}", ctx.time.fps()));
+        let fps_display = graphics::Text::new(format!("FPS: {:.0} Time: {:.0}", ctx.time.fps(), self.time));
         canvas.draw(
             &fps_display,
-            graphics::DrawParam::from([200.0, 0.0]).color(Color::WHITE),
+            graphics::DrawParam::from([10.0, 10.0]).color(Color::WHITE),
         );
 
         canvas.finish(ctx)?;
 
+        Ok(())
+    }
+
+    fn mouse_wheel_event(&mut self, _ctx: &mut Context, _x: f32, y: f32) -> GameResult {
+        self.zoom += y / 20.0;
+        
+        Ok(())
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        button: event::MouseButton,
+        _x: f32,
+        _y: f32
+    ) -> GameResult {
+        if button == event::MouseButton::Left {
+            self.mouse_down = true;
+        }
+
+        Ok(())
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        _ctx: &mut Context,
+        button: event::MouseButton,
+        _x: f32,
+        _y: f32
+    ) -> GameResult {
+        if button == event::MouseButton::Left {
+            self.mouse_down = false;
+        }
+
+        Ok(())
+    }
+
+    fn mouse_motion_event(
+        &mut self,
+        _ctx: &mut Context,
+        _x: f32,
+        _y: f32,
+        dx: f32,
+        dy: f32
+    ) -> GameResult {
+        if self.mouse_down {
+            self.pan = Point2{x: self.pan.x + dx, y: self.pan.y + dy};
+        }
+        
         Ok(())
     }
 }
@@ -89,9 +151,11 @@ pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("points-framer", "ggez")
         .window_setup(WindowSetup::default().title("Factorio Replay Events Points Scatter"))
         .window_mode(
-            WindowMode::default()
-                .dimensions(1500.0, 1500.0)
-                .resizable(true),
+            WindowMode {
+                logical_size: Some(LogicalSize::new(800.0, 600.0)),
+                resizable: true,
+                ..Default::default()
+            },
         );
     let (mut ctx, event_loop) = cb.build()?;
     let state = MainState::new(&mut ctx)?;
